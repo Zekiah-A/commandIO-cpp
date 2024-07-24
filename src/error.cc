f4a9307d2db00748ee46c14b996fe8652bb74456
Zekiah-A <zekiahamoako@outlook.com>
2024-07-24 23:50:55 +0100

    Modernise C++, enum error codes, fix includes, add namespace and cleanups

--- src/error.cc
+++
@@ -1,6 +0,0 @@
-const char* errorMessage[] {
-  "",
-  "Excess parameter: ",
-  "Missing value for parameter ",
-  "Wrong type for parameter ",
-  "Unknown parameter: "};
--- src/error.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-uint8_t const success {0};
-uint8_t const eExcessParam {1};
-uint8_t const eMissingValue {2};
-uint8_t const eParamType {3};
-uint8_t const eUnknownParam {4};
-
-extern const char* errorMessage[];
